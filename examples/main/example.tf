99c8fb5eb94d5b4e2d6af73224c622414ca4208e
Satender Rathee <satender.rathee@ensono.com>
2025-01-15 11:48:59 +0000

    fix: updated

--- examples/main/example.tf
+++ examples/main/example.tf
@@ -7,11 +7,13 @@ module "hub_firewall" {
   azure_location_zones = module.azure_regions.regions_by_name[var.azure_location].zones
   naming_map           = local.name_map["firewall"]
   azure_resource_tags  = local.resource_tags
+
   #Firewall Configurations
   firewall_sku_name                  = "AZFW_VNet"
   firewall_sku_tier                  = "Standard"
   firewall_ip_configuration_subnetid = "/subscriptions/cea31a73-c42c-4b17-b83d-c629142bf942/resourceGroups/rg-ensrtf-eus2-prod-con-hub/providers/Microsoft.Network/virtualNetworks/vnet-ensrtf-eus2-prod-con-hub/subnets/AzureFirewallSubnet"
   # Firewall Policy Configurations
+
   create_firewall_policy                   = true
   firewall_policy_threat_intelligence_mode = "Alert"
   firewall_policy_policy_sku               = "Standard"
